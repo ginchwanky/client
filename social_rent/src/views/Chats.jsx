a40e9187c2084d847140f1156a1910f063fd7188
haekal <haekal.yudh@gmail.com>
2020-03-24 14:14:42 +0700

    added chat component

--- social_rent/src/views/Chats.jsx
+++ social_rent/src/views/Chats.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  { useState, useEffect } from 'react';
 import {
    StyleSheet,
    View,
@@ -9,21 +9,53 @@ import {
    theme,
    Text
 } from 'galio-framework'
+import { GiftedChat } from 'react-native-gifted-chat'
 
 import Constants from 'expo-constants';
 
 const { width, height } = Dimensions.get("screen");
 
 
-export default function Chats(props) {
-
-   return (
-      <>
-      <View style={styles.statusBar}/>
-      <Text muted italic={true} h1>INI PAGE CHATSSSSS</Text>
-      </>
-   )
-}
+export default class Example extends React.Component {
+   state = {
+     messages: [],
+   }
+ 
+   componentDidMount() {
+     this.setState({
+       messages: [
+         {
+           _id: 1,
+           text: 'Hello developer',
+           createdAt: new Date(),
+           user: {
+             _id: 2,
+             name: 'React Native',
+             avatar: 'https://placeimg.com/140/140/any',
+           },
+         },
+       ],
+     })
+   }
+ 
+   onSend(messages = []) {
+     this.setState(previousState => ({
+       messages: GiftedChat.append(previousState.messages, messages),
+     }))
+   }
+ 
+   render() {
+     return (
+       <GiftedChat
+         messages={this.state.messages}
+         onSend={messages => this.onSend(messages)}
+         user={{
+           _id: 1,
+         }}
+       />
+     )
+   }
+ }
 
 const styles = StyleSheet.create({
    statusBar: {
